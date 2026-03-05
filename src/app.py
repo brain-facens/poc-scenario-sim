551ffd6bfc9bcd2ec51d3ce763c7207e618fafc7
Natanco1 <nata.oliveira2004@gmail.com>
2026-03-05 11:22:11 -0300

    feat: comment out more stuff

--- src/app.py
+++ src/app.py
@@ -6,29 +6,29 @@ from fastapi_utilities import repeat_every
 
 from routes import general_routes, simulation_routes, users_routes
 
-@repeat_every(seconds=60)
-async def simulation_watchdog():
-    print("DEBUG: Watchdog cycle started...")
-    """Periodically checks for timeouts and processes the queue."""
-    db = SessionLocal()
-    try:
-        cleanup_count = cleanup_timed_out_simulations(db)
-        promoted_id = await process_stale_queue(db)
-        
-        if cleanup_count > 0 or promoted_id:
-            print(f"Watchdog: Cleaned {cleanup_count} | Promoted {promoted_id}")
-            
-    except Exception as e:
-        print(f"Watchdog Error: {e}")
-    finally:
-        db.close()
+# @repeat_every(seconds=60)
+# async def simulation_watchdog():
+#     print("DEBUG: Watchdog cycle started...")
+#     """Periodically checks for timeouts and processes the queue."""
+#     db = SessionLocal()
+#     try:
+#         cleanup_count = cleanup_timed_out_simulations(db)
+#         promoted_id = await process_stale_queue(db)
+#         
+#         if cleanup_count > 0 or promoted_id:
+#             print(f"Watchdog: Cleaned {cleanup_count} | Promoted {promoted_id}")
+#             
+#     except Exception as e:
+#         print(f"Watchdog Error: {e}")
+#     finally:
+#         db.close()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await simulation_watchdog()
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await simulation_watchdog()
+#     yield
 
-app = FastAPI(title="sql alchemy", lifespan=lifespan)
+app = FastAPI(title="sql alchemy")  # , lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
